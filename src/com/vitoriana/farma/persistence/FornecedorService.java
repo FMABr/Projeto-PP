052d34453826c6665558afd54fe8141cdb2c05ed
Asundersole32 <113726117+Asundersole32@users.noreply.github.com>
2022-10-05 22:56:28 -0400

    Update FornecedorService.java
    Atualização do FuncionarioService

--- src/com/vitoriana/farma/persistence/FornecedorService.java
+++ src/com/vitoriana/farma/persistence/FornecedorService.java
@@ -1,41 +1,56 @@
 package com.vitoriana.farma.persistence;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 
-import com.vitoriana.farma.model.Fornecedor;
 
-public class FornecedorService implements EntidadeService<Fornecedor> {
+import com.vitoriana.farma.model.Funcionario;
 
-	private SerializationService<Fornecedor> serializador = SerializationService.of(Fornecedor.class);
+public class FuncionarioService implements EntidadeService<Funcionario> {
+
+	private SerializationService<Funcionario> serializador = SerializationService.of(Funcionario.class);
 
 	@Override
-	public Fornecedor registrar(Fornecedor entidade) {
-		// TODO Auto-generated method stub
-		return null;
+	public Funcionario registrar(Funcionario entidade) {
+		Funcionario registrado = serializador.serializar(entidade) ? entidade : null;
+		
+		return registrado;
 	}
 
 	@Override
-	public Collection<Fornecedor> listar() {
-		// TODO Auto-generated method stub
-		return null;
+	public Collection<Funcionario> listar() {
+		Funcionario[] funcionario = serializador.desserializarTodos();
+		
+		return new ArrayList<>(Arrays.asList(funcionario));
 	}
 
 	@Override
-	public Fornecedor atualizar(Fornecedor entidade) {
-		// TODO Auto-generated method stub
-		return null;
+	public Funcionario atualizar(Funcionario entidade) {
+		Funcionario atualizado = serializador.serializar(entidade) ? entidade : null;
+
+		return atualizado;
 	}
 
 	@Override
-	public Fornecedor buscar(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Funcionario buscar(int id) {
+		Funcionario funcionario = serializador.desserializar(id);
+
+		return funcionario;
 	}
 
 	@Override
-	public Fornecedor deletar(Fornecedor entidade) {
-		// TODO Auto-generated method stub
-		return null;
+	public Funcionario deletar(Funcionario entidade) {
+		Funcionario deletado = null;
+		try {
+			if (serializador.remover(entidade))
+				deletado = entidade;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		return deletado;
 	}
 
 }
