2bab486045be2bc47938102db6d4c38a9c3973ac
Asundersole32 <113726117+Asundersole32@users.noreply.github.com>
2022-10-07 21:39:21 -0400

    Update FuncionarioService.java
    Atualização da classe de persistencia dos funcionarios

--- src/com/vitoriana/farma/persistence/FuncionarioService.java
+++ src/com/vitoriana/farma/persistence/FuncionarioService.java
@@ -1,5 +1,8 @@
 package com.vitoriana.farma.persistence;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 
 import com.vitoriana.farma.model.Funcionario;
@@ -10,32 +13,43 @@ public class FuncionarioService implements EntidadeService<Funcionario> {
 
 	@Override
 	public Funcionario registrar(Funcionario entidade) {
-		// TODO Auto-generated method stub
-		return null;
+		Funcionario registrado = serializador.serializar(entidade) ? entidade : null;
+		
+		return registrado;
 	}
 
 	@Override
 	public Collection<Funcionario> listar() {
-		// TODO Auto-generated method stub
-		return null;
+		Funcionario[] funcionario = serializador.desserializarTodos();
+		
+		return new ArrayList<>(Arrays.asList(funcionario));
 	}
 
 	@Override
 	public Funcionario atualizar(Funcionario entidade) {
-		// TODO Auto-generated method stub
-		return null;
+		Funcionario atualizado = serializador.serializar(entidade) ? entidade : null;
+
+		return atualizado;
 	}
 
 	@Override
 	public Funcionario buscar(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Funcionario funcionario = serializador.desserializar(id);
+
+		return funcionario;
 	}
 
 	@Override
 	public Funcionario deletar(Funcionario entidade) {
-		// TODO Auto-generated method stub
-		return null;
+		Funcionario deletado = null;
+		try {
+			if (serializador.remover(entidade))
+				deletado = entidade;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		return deletado;
 	}
 
 }
