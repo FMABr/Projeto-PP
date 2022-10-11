048f21e15db74ea20a387589e41751ecc927fcde
Vtaguiar1909 <vitoraguiar141@gmail.com>
2022-10-11 00:20:46 -0400

    Ajustes nos paineis de login e vendas

--- src/com/vitoriana/farma/gui/PainelLogin.java
+++ src/com/vitoriana/farma/gui/PainelLogin.java
@@ -1,30 +1,44 @@
 package com.vitoriana.farma.gui;
 
-import java.awt.Color;
-
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
-public class PainelLogin extends JPanel{
+public class PainelLogin extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	
 	private JTextField login, senha;
+
 	public PainelLogin() {
 		super();
-		setBackground(Color.BLACK);
 		setLayout(null);
-	    login = new JTextField();
+		login = new JTextField();
 		senha = new JTextField();
-		login.setBounds(245,120,350,30);
-		senha.setBounds(245,180,350,30);
+		login.setBounds(245, 120, 350, 30);
+		senha.setBounds(245, 180, 350, 30);
 		add(login);
 		add(senha);
 		JButton entrar = new JButton("Entrar");
-		entrar.setBounds(380,240,80,60);
+		entrar.setBounds(380, 240, 80, 60);
 		add(entrar);
 	}
+
+	// public Funcionario getFuncionario() {
+	// Funcionario login = controller.login(nome.getText(), senha.getText());
+	// return new Funcionario();
+	// }
 	
-	//public Funcionario getFuncionario() {
-		// Funcionario login = controller.login(nome.getText(), senha.getText());
-	//	return new Funcionario();
-	//}
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+			JFrame frame = new JFrame();
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setSize(800, 400);
+			frame.setResizable(false);
+			frame.setVisible(true);
+			frame.add(new PainelLogin());
+		});
+	}
 }
--- src/com/vitoriana/farma/gui/PainelVendas.java
+++ src/com/vitoriana/farma/gui/PainelVendas.java
@@ -1,4 +1,5 @@
 package com.vitoriana.farma.gui;
+
 import java.awt.Color;
 import java.awt.GridLayout;
 
@@ -13,88 +14,82 @@ import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
 
 public class PainelVendas {
-	static JPanel jp = new JPanel(new GridLayout(1,2));
-	
+	JPanel mainPanel = new JPanel(new GridLayout(1, 2));
+
 	public PainelVendas() {
 		JPanel esquerda = new JPanel();
 		esquerda.setBackground(Color.GRAY);
 		esquerda.setLayout(null);
 		JPanel direita = new JPanel();
 		direita.setLayout(null);
-	//esquerda
+		// esquerda
 		JTextField txt1 = new JTextField();
 		esquerda.add(txt1);
-		txt1.setBounds(50,50,200,30);
-		
+		txt1.setBounds(50, 50, 200, 30);
+
 		JButton bt1 = new JButton("Buscar");
 		esquerda.add(bt1);
-		bt1.setBounds(250,49,75,35);
+		bt1.setBounds(250, 49, 75, 35);
 		bt1.setToolTipText("Busca por item");
-		
+
 		JTextField txt2 = new JTextField();
 		esquerda.add(txt2);
-		txt2.setBounds(20,200,200,22);
+		txt2.setBounds(20, 200, 200, 22);
 		txt2.setEditable(false);
-		
+
 		JTextField txt3 = new JTextField();
 		esquerda.add(txt3);
-		txt3.setBounds(20,225,200,22);
+		txt3.setBounds(20, 225, 200, 22);
 		txt3.setEditable(false);
-		
+
 		JTextField txt4 = new JTextField();
 		esquerda.add(txt4);
-		txt4.setBounds(20,250,200,22);
+		txt4.setBounds(20, 250, 200, 22);
 		txt4.setEditable(false);
-		
+
 		JButton btCmpr = new JButton("Comprar");
 		esquerda.add(btCmpr);
-		btCmpr.setBounds(240,477,135,80);
-		
-		JSpinner sp = new JSpinner(new SpinnerNumberModel(1,0,100,1));
+		btCmpr.setBounds(240, 477, 135, 80);
+
+		JSpinner sp = new JSpinner(new SpinnerNumberModel(1, 0, 100, 1));
 		esquerda.add(sp);
 		sp.setBounds(100, 477, 125, 80);
-		
-	//direita
+
+		// direita
 		JButton btF = new JButton("Finalizar Compra");
 		direita.add(btF);
-		btF.setBounds(240,475,145,80);
-		
+		btF.setBounds(240, 475, 145, 80);
+
 		JTable tab2 = new JTable();
 		JScrollPane tab = new JScrollPane(tab2);
 		direita.add(tab);
 		tab.setBackground(Color.black);
-		tab.setBounds(45,30,300,300);
-	
-		
+		tab.setBounds(45, 30, 300, 300);
+
 		JLabel l1 = new JLabel("quantidade:");
 		direita.add(l1);
-		l1.setBounds(30,380,80,20);
+		l1.setBounds(30, 380, 80, 20);
 		JLabel l2 = new JLabel("valor total:");
 		direita.add(l2);
-		l2.setBounds(30,420,80,20);
-		
+		l2.setBounds(30, 420, 80, 20);
+
 		JTextField qntdItens = new JTextField();
 		direita.add(qntdItens);
-		qntdItens.setBounds(100,380,200,30);
-		
+		qntdItens.setBounds(100, 380, 200, 30);
+
 		JTextField valorTotal = new JTextField();
 		direita.add(valorTotal);
-		valorTotal.setBounds(100,420,200,30);
-	//finally
-		jp.add(esquerda);
-		jp.add(direita);
-		
-	//daqui pra baixo é teste	
-		JFrame jf = new JFrame();
-		jf.add(jp);
-		jf.setSize(800, 600);
-		jf.setVisible(true);
+		valorTotal.setBounds(100, 420, 200, 30);
+		// finally
+		mainPanel.add(esquerda);
+		mainPanel.add(direita);
+
 	}
-	
-	public JPanel mainPanel() {
-		return jp;
+
+	public JPanel getMainPanel() {
+		return mainPanel;
 	}
-	
+
 	public static void main(String args[]) {
 		PainelVendas pv = new PainelVendas();
 	}
